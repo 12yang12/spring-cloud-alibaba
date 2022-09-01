edd97fece36a2dd8229bcf227143e7c076188c0d
12yang12 <18800122159@163.com>
2022-09-01 15:08:50 +0800

    UserMapper

--- mybatis-spring-boot/src/main/java/com/yang/dao/UserMapper.java
+++ mybatis-spring-boot/src/main/java/com/yang/dao/UserMapper.java
@@ -10,13 +10,13 @@ import java.util.List;
 @Repository
 public interface UserMapper {
 
-    public List<User> findAllUser();
+    List<User> findAllUser();
 
-    public User findUserById(Integer id);
+    User findUserById(Integer id);
 
-    public int insertUser(User user);
+    int insertUser(User user);
 
-    public int updateUser(User user);
+    int updateUser(User user);
 
-    public int deleteById(Integer id);
+    int deleteUserById(Integer id);
 }
