45a113cb626bf35c22924eb3533cf9241b2a1cf8
fanGY <245379383@qq.com>
2022-09-13 07:41:13 +0800

    sentinel-dashboard

---
+++ sentinel-dashboard/src/main/java/com/yang/SentinelDashboardMain.java
@@ -0,0 +1,13 @@
+package com.yang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelDashboardMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelDashboardMain.class, args);
+        System.out.println("Hello world!");
+    }
+}
