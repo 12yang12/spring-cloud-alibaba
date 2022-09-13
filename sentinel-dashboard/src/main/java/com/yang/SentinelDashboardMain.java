972362d03bc3baadcfb0b15664a14168e45a1f1a
fanGY <245379383@qq.com>
2022-09-13 22:11:38 +0800

    sentinel dashboard

--- sentinel-dashboard/src/main/java/com/yang/SentinelDashboardMain.java
+++ sentinel-dashboard/src/main/java/com/yang/SentinelDashboardMain.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SentinelDashboardMain {
-
     public static void main(String[] args) {
         SpringApplication.run(SentinelDashboardMain.class, args);
         System.out.println("Hello world!");
--- sentinel-dashboard/src/main/java/com/yang/controller/SentinelController.java
+++ sentinel-dashboard/src/main/java/com/yang/controller/SentinelController.java
@@ -1,15 +1,30 @@
 package com.yang.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SentinelController {
 
-    @GetMapping(value = "/hello")
-    @SentinelResource("hello")
-    public String hello() {
+    @GetMapping(value = "/hello/{name}")
+    @SentinelResource(value = "hello", blockHandler = "helloBlockHandler", fallback = "helloFallback")
+    public String hello(@PathVariable String name) throws Exception {
+        if ("1".equals(name)) {
+            throw new Exception("输入1");
+        }
         return "Hello Sentinel";
+    }
+
+    public String helloBlockHandler(String name, BlockException blockException) {
+        System.out.println("Sentinel 限流");
+        return "Sentinel blockHandler";
+    }
+
+    public String helloFallback(String name, Throwable throwable) {
+        System.out.println("Sentinel 熔断");
+        return "Sentinel helloFallback";
     }
 }
