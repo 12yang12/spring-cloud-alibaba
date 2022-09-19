2fe4bd6d16494690de761755490361c73b875ef3
fanGY <245379383@qq.com>
2022-09-19 21:02:06 +0800

    nacos+RefreshScope

--- nacos-config/src/main/java/com/yang/NacosConfigApplication.java
+++ nacos-config/src/main/java/com/yang/NacosConfigApplication.java
@@ -2,18 +2,22 @@ package com.yang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class NacosConfigApplication {
 
     public static void main(String[] args) throws InterruptedException {
-       SpringApplication.run(NacosConfigApplication.class, args);
-        // ConfigurableApplicationContext applicationContext =
-        // while (true) {
-        //     //当动态配置刷新时，会更新到 Enviroment中，因此这里每隔一秒中从Enviroment中获取配置
-        //     String name = applicationContext.getEnvironment().getProperty("name");
-        //     System.err.println("name :" + name );
-        //     TimeUnit.SECONDS.sleep(1);
-        // }
+
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosConfigApplication.class, args);
+
+        while (true) {
+            // 当动态配置刷新时会更新到Enviroment中，因此这里每隔5秒中从Enviroment中获取配置
+            String name = applicationContext.getEnvironment().getProperty("name");
+            System.out.println("name: " + name);
+            TimeUnit.SECONDS.sleep(5);
+        }
     }
 }
