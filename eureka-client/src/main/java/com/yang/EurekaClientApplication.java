4bfb5e43333e18405e57075f333d5e509d23d16d
fanGY <245379383@qq.com>
2022-09-24 22:49:51 +0800

    eureka client

--- eureka-client/src/main/java/com/yang/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/yang/EurekaClientApplication.java
@@ -2,13 +2,12 @@ package com.yang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaClientApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(EurekaClientApplication.class, args);
         System.out.println("Hello world!");
     }
 }
