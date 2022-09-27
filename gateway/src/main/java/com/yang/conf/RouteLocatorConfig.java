1e4abd47eb68391ee2461bb8f0f1cb1cf421e303
fanGY <245379383@qq.com>
2022-09-28 07:17:31 +0800

    gateway编码实现路由

---
+++ gateway/src/main/java/com/yang/conf/RouteLocatorConfig.java
@@ -0,0 +1,19 @@
+package com.yang.conf;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteLocatorConfig {
+
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/user/**")
+                        .uri("lb://user-service"))
+                .build();
+    }
+}
