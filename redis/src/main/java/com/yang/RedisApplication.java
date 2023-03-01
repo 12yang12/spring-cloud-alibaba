826321c43d9d822ae935c691478cd6d56740d51e
fgaoyang <18800122159@163.com>
2023-03-01 21:38:21 +0800

    Redis

--- redis/src/main/java/com/yang/RedisApplication.java
+++ redis/src/main/java/com/yang/RedisApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RedisApplication {
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
-        System.out.println("Hello world!");
+        System.out.println("Hello Redis!");
     }
 }
